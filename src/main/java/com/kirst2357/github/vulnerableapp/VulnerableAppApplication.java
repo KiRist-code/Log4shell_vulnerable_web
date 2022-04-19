fdf7ab27de10b3d842ad0b40c33b8b81c74d4e58
KiRist-code <minjun9886@naver.com>
2022-04-19 18:47:36 +0900

    initial commit

---
+++ src/main/java/com/kirst2357/github/vulnerableapp/VulnerableAppApplication.java
@@ -0,0 +1,13 @@
+package com.kirst2357.github.vulnerableapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerableAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VulnerableAppApplication.class, args);
+    }
+
+}
